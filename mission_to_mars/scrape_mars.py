0e68ed42a49722ce4703f9ba3c9997c7c3aa8a39
alisonlitton <63117075+alisonlitton@users.noreply.github.com>
2020-08-23 20:57:14 -0700

    updating crap

--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -1,6 +1,8 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
+import pandas as pd
+import requests 
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
@@ -12,57 +14,51 @@ def scrape():
     browser = init_browser()
 
     # Visit MarsNews
-    url = "https://mars.nasa.gov/news/"
-    browser.visit(url)
+    news_url = "https://mars.nasa.gov/news/"
+    browser.visit(news_url)
     time.sleep(1)
     # Scrape page into Soup
     html = browser.html
-    soup = bs(html, "html.parser")
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    news_soup = bs(html, "html.parser")
+
+
     # Get the latest news title and paragraph
-    news_titles = soup.find_all('div', class_='content_title')
-    paragraph_text = soup.find_all('div', class_='article_teaser_body')
-    news_title = news_titles[0].text
-    news_p = paragraph_text[0].text
+    news_title = news_soup.find('div', class_='content_title').get_text()
+    p_text = news_soup.find('div', class_='article_teaser_body').get_text()
 
 
     #Visit JPL
-    browser.visit('https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
+    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(jpl_url)
     time.sleep(1)
-    #click to get full image
-    browser.click_link_by_partial_text('FULL IMAGE')
-    time.sleep(1)
-    browser.click_link_by_partial_text('more info')
-    #scrape page into soup
     html = browser.html
-    soup = bs(html, 'html.parser')
-    #find the featured image 
-    img_result = soup.find_all('figure', class_='lede')
-    img_path = img_result[0].a['href']
-    feat_img = 'https://www.jpl.nasa.gov/spaceimages' + img_path
+    jpl_soup = bs(html, "html.parser")
 
+    #find the featured image 
+    img_path = jpl_soup.find('a', class_='button fancybox').get('data-fancybox-href')
+    feat_img = jpl_url + img_path
 
     #visit MarsFacts
     mars_facts_url = 'https://space-facts.com/mars/'
-    tables = pd.read_html(mars_facts_url)
+    browser.visit(mars_facts_url)
     time.sleep(1)
-    df = tables[0]
-    df.columns = ['Fact', 'Value']
-    mars_table = df.to_html()
+    mars_facts = pd.read_html(mars_facts_url)
+    df = mars_facts[0]
+    mars_facts_df = df.rename(columns={0: "", 1: "Mars"})
+    html_table = mars_facts_df.to_html()
+    html_table.replace('\n', '')
+    mars_table = mars_facts_df.to_html('mars_facts.html', index=False)
+   
 
     #visit MarsHemi page
-    browser.visit('https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars')
-    time.sleep(1)
-    html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
-    hemisphere_names = []
-    hemi_results = soup.find_all('div', class_='collapsible results')
-    hemispheres = hemi_results[0].find_all('h3')
-    for name in hemispheres:
-        hemisphere_names.append(name.text)  
-    thumb_results = hemi_results[0].find_all('a')
-    thumb_links = []
+    hemi_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    urls = [
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced',
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced',
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced',
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
+        ]
+    
 
     for thumb in thumb_results:
         if (thumb.img):
